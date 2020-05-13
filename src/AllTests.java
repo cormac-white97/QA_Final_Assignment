4624e3d8028b53a0ef22c8a553eb3e4bbaf97c63
Cormac White <Cormac.White@evros.ie>
2020-05-13 23:14:51 +0100

    Test to add a survey

--- src/AllTests.java
+++ src/AllTests.java
@@ -4,4 +4,15 @@ import org.junit.Test;
 
 public class AllTests {
 	Survey s;
+	
+	@Test
+	public void createSurvey() {
+		//Create a new Survey object and pass in the title
+		Survey s = new Survey("Quality Survey");
+		
+		//Chck that the title is equal to the one that was passed in when it was created
+		assertEquals("Quality Survey", s.getTitle());
+	}
+	
+	
 }
