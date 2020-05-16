3aa76f0ee0e5ad1b73932ded2da3a8f6a24e75f4
Cormac White <Cormac.White@evros.ie>
2020-05-16 15:41:37 +0100

    adding ability to add survey questions

--- src/AllTests.java
+++ src/AllTests.java
@@ -4,15 +4,27 @@ import org.junit.Test;
 
 public class AllTests {
 	Survey s;
-	
+
 	@Test
 	public void createSurvey() {
-		//Create a new Survey object and pass in the title
+		// Create a new Survey object and pass in the title
 		Survey s = new Survey("Quality Survey");
-		
-		//Chck that the title is equal to the one that was passed in when it was created
+
+		// Chck that the title is equal to the one that was passed in when it was
+		// created
 		assertEquals("Quality Survey", s.getTitle());
 	}
-	
-	
+
+	@Test
+	public void addQuestions() {
+		Survey s = new Survey("Quality Survey");
+
+		s.addQuestions("Customer Service");
+		s.addQuestions("Hygine");
+
+		assertEquals("Customer Service", s.getQuestions().get(0));
+		assertEquals("Hygine", s.getQuestions().get(1));
+	}
+
+
 }
